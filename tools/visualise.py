2090da502bdd7d98b56af3cb13992e32fb7ce116
teethoe <ardithhowaiman@gmail.com>
2023-10-30 15:08:09 +0000

    add logger for classes detected

--- tools/visualise.py
+++ tools/visualise.py
@@ -11,13 +11,56 @@ import argparse
 import pickle
 import torch
 import io
+import numpy as np
+import yaml
+from pathlib import Path
+from easydict import EasyDict
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg_file', type=str, default='cfgs/kitti_models/pointpillar.yaml',
+                        help='specify the config for demo')
     parser.add_argument('--data_path', type=str, default='demo_data',
                         help='specify the result data file or directory')
-    return parser.parse_args()
+    args = parser.parse_args()
+    cfg = EasyDict()
+    cfg.ROOT_DIR = (Path(__file__).resolve().parent / '../').resolve()
+    cfg.LOCAL_RANK = 0
+    cfg_from_yaml_file(args.cfg_file, cfg)
+    return args, cfg
+
+
+def merge_new_config(config, new_config):
+    if '_BASE_CONFIG_' in new_config:
+        with open(new_config['_BASE_CONFIG_'], 'r') as f:
+            try:
+                yaml_config = yaml.safe_load(f, Loader=yaml.FullLoader)
+            except:
+                yaml_config = yaml.safe_load(f)
+        config.update(EasyDict(yaml_config))
+
+    for key, val in new_config.items():
+        if not isinstance(val, dict):
+            config[key] = val
+            continue
+        if key not in config:
+            config[key] = EasyDict()
+        merge_new_config(config[key], val)
+
+    return config
+
+
+def cfg_from_yaml_file(cfg_file, config):
+    with open(cfg_file, 'r') as f:
+        try:
+            new_config = yaml.safe_load(f, Loader=yaml.FullLoader)
+        except:
+            new_config = yaml.safe_load(f)
+
+        merge_new_config(config=config, new_config=new_config)
+
+    return config
 
 
 class CpuUnpickler(pickle.Unpickler):
@@ -29,10 +72,13 @@ class CpuUnpickler(pickle.Unpickler):
 
 
 def main():
-    args = parse_opt()
+    args, cfg = parse_opt()
     with open(args.data_path, 'rb') as f:
         data_dict = CpuUnpickler(f).load()
         pred_dicts = CpuUnpickler(f).load()
+        labels = np.unique(np.array(pred_dicts[0]['pred_labels']))
+        for label in labels:
+            print(f'Found {list(pred_dicts[0]["pred_labels"]).count(label)} {cfg.CLASS_NAMES[label]}(s)')
         V.draw_scenes(
             points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
             ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
